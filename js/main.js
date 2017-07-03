ce14a29d2796763ff8f29ee7857714cf0dc921e8
vanita22 <vanitayrafita2004@gmail.com>
2017-07-02 23:25:46 -0300

    subiendo archivo en jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
-function validateForm(){
-	/* Escribe tú código aquí */
-}
+/*function validateForm(){
+	 Escribe tú código aquí */
+	$(document).ready(function(){
+		/*var mail = /^[A-Z_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;*/
+		$("#registrar").click(function(){
+			var nombre = $("#name").val();
+			var apellido = $("#lastname").val();
+			var email = $("#input-email").val();
+			var password = $("#input-password").val();
+
+			if(nombre === ""){
+				alert("ingrese nombre");
+				return false;				
+			}
+			if(nombre != nombre.upperCase(A-Z_)){
+				alert("ingrese solo nombre con mayúscula");
+				return false;				
+			}
+			if(apellido === ""){
+				alert("ingrese nombre");
+				return false;				
+			}
+			if(apeliido != apellido.upperCase(A-Z_)){
+				alert("ingrese solo apellido con mayúscula");
+				return false;				
+			}
+			if(email === ""){
+				alert("ingrese nombre");
+				return false;				
+			}
+			if(email != /^[A-Z_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/){
+				alert("ingrese email válido");
+				return false;				
+			}
+			if(password === ""){
+				alert("ingrese contraseña");
+				return false;				
+			}
+			if(password.lenght < 6){
+				alert("ingrese password mayor a 6 letras y números");
+				return false;				
+			}
+			if(password === "123456" || password === "098765" || password === "password"){
+				alert("ingrese password con letras y números distintos a 123456, 098765 y password");
+				return false;				
+			}
+		})
+	});
+/*}*/
